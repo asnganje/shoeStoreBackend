2014d41078c33b1179b7c2451dcfdced59265d4f
asnganje <abd.nganje@gmail.com>
2024-02-03 21:13:32 +0300

    Add bcrypt encryption logic when creating user

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     firstName: {
@@ -18,6 +19,11 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide last name']
     }
+})
+
+UserSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 
