6910ae9817697b610e665d4306b6673303518536
asnganje <abd.nganje@gmail.com>
2024-02-03 21:30:07 +0300

    use cors policy in app

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const connectDB = require('./server/db/connect')
 const userRouter = require('./server/routes/userRoutes')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use('/api/v1/shoestore', userRouter)
 
 const port = process.env.PORT;
