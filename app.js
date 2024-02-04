cfb2477ac3414e8c86f14060d2ad2dc3ae2b5aac
asnganje <abd.nganje@gmail.com>
2024-02-04 16:46:30 +0300

    Add products and stores routers

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const connectDB = require('./server/db/connect')
 const userRouter = require('./server/routes/userRoutes')
+const productsRouter = require('./server/routes/productsRoutes')
+const storeRouter = require('./server/routes/storeRoutes')
 const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use('/api/v1/shoestore', userRouter)
+app.use('/api/v1/shoestore', productsRouter)
+app.use('/api/v1/shoestore', storeRouter)
 
 const port = process.env.PORT;
 const url = process.env.MONGO_URI
