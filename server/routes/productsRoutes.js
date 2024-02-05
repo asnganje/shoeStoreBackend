c28606737dc4cae84d0617c88428a12fe197ec61
asnganje <abd.nganje@gmail.com>
2024-02-05 21:30:31 +0300

    modify productsroutes logic

--- server/routes/productsRoutes.js
+++ server/routes/productsRoutes.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const { getByStoreId, getProductsByStoreId, createProduct, deleteProduct, updateProduct } = require('../controllers/productsController')
-const router = express.Router()
+const express = require("express");
+const {
+  getByStoreId,
+  getProductsByStoreId,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/productsController");
+const router = express.Router();
 
-router.route('/products').post(createProduct).get(getProductsByStoreId).delete(deleteProduct).patch(updateProduct)
+router
+  .route("/products")
+  .post(createProduct)
+  .get(getProductsByStoreId)
+  .delete(deleteProduct)
+  .patch(updateProduct);
 
 module.exports = router;
